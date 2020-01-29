39b84ef80d802c7bb191cfffdccc00db8fa0fd98
jarek3 <44277785+jarek3@users.noreply.github.com>
2020-01-29 22:12:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ mb_internal_encoding("UTF-8");
 function autoloadFunkce($class)
 {
         // Končí název třídy řetězcem "Kontroler" ?
-        if (preg_match('/Controller/', $class))
+        if (preg_match('/Controller$/', $class))
                 require("controllers/" . $class . ".php");
         else
                 require("models/" . $class . ".php");
@@ -16,4 +16,4 @@ Db::connect("sql2.webzdarma.cz", "evidencewzsk0356", "***", "evidencewzsk0356");
 $router = new RouterController();
 $router->process(array($_SERVER['REQUEST_URI']));
 $router->renderView();
-?> 
+?>
